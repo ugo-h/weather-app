ab125dbb2dd67f7ed96b160b93c4c9ecad3ec178
ugo-h <s_per00@outlook.com>
2021-01-12 17:58:16 +0300

    Исправил баг с undefined вместо названия города (у некоторых локаций нету поля city, но есть поле town)

--- src/lib/assembleLocation.js
+++ src/lib/assembleLocation.js
@@ -1,3 +1,3 @@
 export function assembleLocation(components) {
-    return (components.city || components.state) + ', ' + components.country;
+    return (components.city || components.town || components.state) + ', ' + components.country;
 }
